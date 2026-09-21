36d9a7248531b2aabe79451df8640c8fcdf01da8
sanjaytuti <sanjaytuti787@gmail.com>
2026-09-21 10:11:05 +0530

    Add UserProfile object with user constants

---
+++ app/src/main/java/com/example/era/core/UserProfile.kt
@@ -0,0 +1,8 @@
+package com.example.era.core
+
+object UserProfile {
+    const val USER_NAME = "Sanju"
+    const val NICKNAME = "Sanju"
+    const val ADDRESS_AS = "Sanju"
+    const val JEALOUS_NUMBER = "8987502936"
+}
